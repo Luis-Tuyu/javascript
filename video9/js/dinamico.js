38be7acc6c75545b2c1d22abeb5876416e2db704
LTUYU <franciscotuyupadron@gmail.com>
2020-01-01 18:35:30 -0600

    video 9,  manipulando EL DOM con js, hay un ejemplo, como si fuese de manera formal todo como un página web

---
+++ video9/js/dinamico.js
@@ -0,0 +1,7 @@
+var div2= document.getElementById("mi_div");
+console.log(div2);
+var div=document.getElementsByClassName("tit2");
+console.log(div);
+//div.style.color= red;
+
+console.log("Aún no se como usarlo en JS, pero obtenerlo si lo sé")
