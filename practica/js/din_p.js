cfc3fc2e04b5bd2ed2960e724e5682b256194905
LTUYU <franciscotuyupadron@gmail.com>
2020-01-14 11:42:43 -0600

    práctica lista, cambio de colores(botones,seleccion y promt)

--- practica/js/din_p.js
+++ practica/js/din_p.js
@@ -1,7 +1,7 @@
 //cambia el color del fondo
 function colorchange()
 {var seleccion = document.getElementById("myseleccion").value;
-document.getElementById("texto").innerHTML = "seleccionaste: "+ seleccion;
+document.getElementById("texto").innerHTML = "Seleccionaste: "+ seleccion;
 document.body.style.backgroundColor = seleccion;
 }
 //cambia el color del texto
@@ -10,14 +10,14 @@ function changecolor_text()
 t.style.color = colortxt;
 }
 //ejecutamos el prompt
-var dato = prompt("ingresa un color","0");
+//var dato = prompt("ingresa un color","0");
 //console.log(dato);
-var resultado = changecolor2_text(dato);
+//var resultado = changecolor2_text(dato);
 //cambiar el color del texto con prompt, c será el texto ingresado en prompt
 function changecolor2_text(c)
 { 
     if(c == "rojo")
-    {text2.style.color = "red";
+    {text2.style.color = "#EB1440";
     document.getElementById("mensaje").innerHTML = "color: "+c;
     }else if(c == "verde"){
         text2.style.color = "green";
@@ -26,4 +26,9 @@ function changecolor2_text(c)
         //lo pondremos en una etiqueta html
         document.getElementById("mensaje").innerHTML = "ERROR, color no válido";
     }  
+}
+
+//con botones
+function changecolor_boton(color)
+{document.body.style.backgroundColor =  color;
 }
