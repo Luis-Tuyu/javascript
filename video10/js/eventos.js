fd49e7d01ff690a7740c5018e326990bd7f7680b
LTUYU <franciscotuyupadron@gmail.com>
2020-01-14 17:39:33 -0600

    practicando con listeners, carpeta video 10

--- video10/js/eventos.js
+++ video10/js/eventos.js
@@ -4,13 +4,20 @@ function clic() {
 
 //del segundo boton donde esta el div
 //nombre del id         //  click, funcion, condicion
-document.getElementById("b1").addEventListener("click", clic2, true);
-document.getElementById("mi_div").addEventListener("click", another_clic, false)
-
+document.getElementById("b1").addEventListener("click", clic2,true);
+document.getElementById("mi_div").addEventListener("click", another_clic,false)
+//agregagmos un listener para cambiar de fondo con listener
+document.getElementById("b2").addEventListener("click", change_color);
 function clic2() {
     alert("alerta2");
 }
 
 function another_clic() {
     alert("esto fue disparado por el otro botón");
+}
+
+function change_color()
+{
+    document.body.style.backgroundColor = "red";
+    console.log("cambio de de fondo a rojo");
 }
