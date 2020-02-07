4614ce3cfffc3705fb8930e38285af937e0efb79
Luis-Tuyu <e17081482@itmerida.edu.mx>
2020-02-06 19:07:11 -0600

    video25, tercera parte del pin_ping

--- video23_practica_mvc_parte1/js/main.js
+++ video23_practica_mvc_parte1/js/main.js
@@ -28,17 +28,22 @@
                 this.height = height;
                 this.board = board;
                 this.board.bars.push(this); //accedo al board, accedo al bpard y le agrego este objeto
+                console.log("elementos: ", board.elements.lenght);
                 this.kind = "rectangle";
                 console.log("hola contructormm");
+                this.speed = 10;
             }
             //modificamos los elementos de una funcio con el prototype
             //además estamos incluyendo un json con funciones
         self.Bar.prototype = {
             down: function() {
-
+                this.y += this.speed; //aumentar a la cordenada en y
             },
             up: function() {
-
+                this.y -= this.speed;
+            },
+            toString: function() {
+                return "x: " + this.x + "y: " + this.y;
             }
         }
     }
@@ -50,6 +55,7 @@
             this.canvas.width = board.width;
             this.canvas.height = board.height;
             this.board = board;
+            console.log("board " + board.elements);
             this.ctx = canvas.getContext("2d");
             console.log("entrando a board vuew")
         }
@@ -58,8 +64,12 @@
 
     self.BoardView.prototype = {
         draw: function() {
+            console.log("entreo al protoryé");
+            //console.log("# de elementos" + this.board.elements.lenght);
+            //el error es que board.elements da null
             for (var i = this.board.elements.lenght - 1; i >= 0; i--) {
                 var el = this.board.elements[i];
+                console.log("imprimiendo el:" + el);
                 console.log("ingresando al protoitpo");
                 draw(this.ctx, el);
             }
@@ -82,13 +92,24 @@
 
 })();
 
-window.addEventListener("load", main);
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 50, board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas, board);
+
+document.addEventListener("keydown", function(ev) {
+    console.log(ev.keyCode);
+    if (ev.keyCode == 38) {
+        bar.up();
+    } else if (ev.keyCode == 40) {
+        bar.down();
+    }
+    console.log(bar.toString());
+});
+self.addEventListener("load", main);
 
 function main() {
 
-    var board = new Board(800, 400);
-    var bar = new Bar(20, 100, 40, 50, board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas, board);
+
     board_view.draw();
 }
